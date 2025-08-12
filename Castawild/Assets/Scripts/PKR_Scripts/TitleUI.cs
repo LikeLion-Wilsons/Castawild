8c9ed9d2d901ba19dee89b8f2dc5f2823e973004
박기락 <kirak0622@gmail.com>
2025-08-12 12:32:01 +0900

    titleUI 작업

--- Castawild/Assets/Scripts/PKR_Scripts/TitleUI.cs
+++ Castawild/Assets/Scripts/PKR_Scripts/TitleUI.cs
@@ -12,57 +12,63 @@ namespace Test
     {
         public NetworkRunner RunnerPrefab;
         public GameObject rootUI;
-        public TextMeshProUGUI StatusText;
-        public GameObject startGroup;
-        public GameObject disconnectGroup;
-        public CanvasGroup panelGroup;
-        
-        public TMP_InputField sessionnameInput;
-        public TMP_InputField nicknameInput;
-        public Toggle singleToggle;
+
+        [Header("Page0")]
+        [SerializeField] GameObject rootPage0;
+        [SerializeField] Button btnStart;
+        [SerializeField] Button btnOption;
+        [SerializeField] Button btnExit;
+        //-----
+
+        [Header("Page1")]
+        [SerializeField] GameObject rootPage1;
+        [SerializeField] TMP_InputField sessionNameInput;
+        [SerializeField] TMP_InputField nicknameInput;
+        [SerializeField] Toggle singleToggle;
+        [SerializeField] Toggle multiToggle;
+        [SerializeField] Button btnBack;
+        [SerializeField] Button btnOK;
+        [SerializeField] TextMeshProUGUI StatusText;
+        //-----
+
         private NetworkRunner _runner;
         private static string _shutdownStatus;
 
-        private void OnEnable()
+        void Awake()
         {
-            //닉네임 임시저장.
+            //닉네임.
             var nickname = PlayerTempData.nickname;
             if (string.IsNullOrEmpty(nickname))
             {
                 nickname = "Player" + Random.Range(10000, 100000);
             }
-            nicknameInput.text = nickname;
-            
-            //세션네임.
-            sessionnameInput.text = "TestRoom";
-            
-            // Try to load previous shutdown status
-            StatusText.text = _shutdownStatus != null ? _shutdownStatus : string.Empty;
-            _shutdownStatus = null;
-        }
-        
-        private void Update()
-        {
-            if (panelGroup.gameObject.activeSelf)
-            {
-                startGroup.SetActive(_runner == null);
-                disconnectGroup.SetActive(_runner != null);
 
-                //Cursor.lockState = CursorLockMode.None;
-                //Cursor.visible = true;
-            }
-            else
-            {
-                //Cursor.lockState = CursorLockMode.Locked;
-                //Cursor.visible = false;
-            }
+            nicknameInput.text = nickname;
+
+            //세션.
+            sessionNameInput.text = "TestRoom";
+            StatusText.text = string.Empty;
+            _shutdownStatus = null;
+
+            //Page0.
+            SetPage(0);
+            btnStart.onClick.AddListener(() => SetPage(1));
+            btnExit.onClick.AddListener(OnClickExit);
+            
+            //page1.
+            btnBack.onClick.AddListener(() => SetPage(0));
+            btnOK.onClick.AddListener(StartGame);
         }
         public async void StartGame()
         {
-            await Disconnect();
-            PlayerTempData.nickname = nicknameInput.text;
+            btnBack.interactable = false;
+            btnOK.interactable = false;
+            singleToggle.interactable = false;
+            multiToggle.interactable = false;
             nicknameInput.interactable = false;
-            sessionnameInput.interactable = false;
+            sessionNameInput.interactable = false;
+            PlayerTempData.nickname = nicknameInput.text;
+            
             _runner = GameObject.Instantiate(RunnerPrefab);
             var events = _runner.GetComponent<NetworkEvents>();
             events.OnShutdown.AddListener(OnShutdown);
@@ -74,16 +80,16 @@ namespace Test
             {
                 GameMode = singleToggle.isOn ? GameMode.Single : GameMode.AutoHostOrClient,
                 Scene = sceneInfo,
-                SessionName = sessionnameInput.text,
+                SessionName = sessionNameInput.text,
                 //PlayerCount = MaxPlayerCount,
             };
-            
+
             StatusText.text = "Connecting...";
 
 
             var task = _runner.StartGame(startArguments);
             await task;
-            
+
             if (task.Result.Ok)
             {
                 StatusText.text = "";
@@ -94,29 +100,8 @@ namespace Test
                 Debug.Log($"Connection Failed: {task.Result.ShutdownReason}");
             }
         }
-        public async void DisconnectClicked()
-        {
-            await Disconnect();
-        }
-        
-        public async Task Disconnect()
-        {
-            if (_runner == null)
-                return;
 
-            StatusText.text = "Disconnecting...";
-            panelGroup.interactable = false;
 
-            // Remove shutdown listener since we are disconnecting deliberately
-            var events = _runner.GetComponent<NetworkEvents>();
-            events.OnShutdown.RemoveListener(OnShutdown);
-
-            await _runner.Shutdown();
-            _runner = null;
-
-            // Reset of scene network objects is needed, reload the whole scene
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
         private void OnShutdown(NetworkRunner runner, ShutdownReason reason)
         {
             // Unexpected shutdown happened (e.g. Host disconnected)
@@ -127,6 +112,22 @@ namespace Test
 
             // Reset of scene network objects is needed, reload the whole scene
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+        
+        private void OnClickExit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+        private void SetPage(int page)
+        {
+            rootPage0.SetActive(page == 0);
+            rootPage1.SetActive(page == 1);
         }
     }
 }
