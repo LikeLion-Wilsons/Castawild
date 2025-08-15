bc399c8e25606c10cfb1907115cec2aa2a0bc845
chlghksgml01 <chlghksgml01@naver.com>
2025-08-15 21:37:07 +0900

    25/08/15 [fix] 인덱스 수정

--- Castawild/Assets/Scripts/CHH_Scripts/Player/FootstepCurveReceiver.cs
+++ Castawild/Assets/Scripts/CHH_Scripts/Player/FootstepCurveReceiver.cs
@@ -20,7 +20,7 @@ public class FootstepCurveReceiver : MonoBehaviour
 
     void Update()
     {
-        int randomNumber = UnityEngine.Random.Range(0, 8);
+        int randomNumber = UnityEngine.Random.Range(0, 7);
         Sound[] walkSounds = { Sound.Player_Sleep3, Sound.Player_Sleep3 };
 
         if (step >= threshold && prevStep < threshold)
