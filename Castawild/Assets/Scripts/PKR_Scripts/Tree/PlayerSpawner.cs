6d2d8a5637cfbd9a7e412a6369f38c157f48b77f
chlghksgml01 <chlghksgml01@naver.com>
2025-08-17 21:12:15 +0900

    25/08/17 [chore] 시작위치 수정

--- Castawild/Assets/Scripts/PKR_Scripts/Tree/PlayerSpawner.cs
+++ Castawild/Assets/Scripts/PKR_Scripts/Tree/PlayerSpawner.cs
@@ -3,20 +3,20 @@ using UnityEngine;
 
 namespace Test
 {
-    
+
     public class PlayerSpawner : NetworkBehaviour, IPlayerJoined, IPlayerLeft
     {
         [SerializeField] private NetworkObject PlayerPrefab;
+        [SerializeField] private Transform spawnPosition;
 
         public void PlayerJoined(PlayerRef playerRef)
         {
             if (HasStateAuthority == false) return;
 
-            var x= UnityEngine.Random.Range(-3f, 0f);
-            var z= UnityEngine.Random.Range(-3f, 3f);
-            var spawnPosition = transform.position + new Vector3(x, 3f, z);
+            var x = UnityEngine.Random.Range(-3f, 0f);
+            var z = UnityEngine.Random.Range(-3f, 3f);
 
-            var playerObj = Runner.Spawn(PlayerPrefab, spawnPosition, Quaternion.identity, playerRef, (runner, o) =>
+            var playerObj = Runner.Spawn(PlayerPrefab, spawnPosition.position, Quaternion.identity, playerRef, (runner, o) =>
             {
                 //o.GetComponent<Player>().Init();
             });
